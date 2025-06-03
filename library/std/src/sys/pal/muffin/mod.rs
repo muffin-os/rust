2bf91f2540157ae41de7b8ac60d3559a208a9787
tsatke <tim.satke@gmail.com>
2025-06-03 17:56:38 +0200

    add call to main

--- library/std/src/sys/pal/muffin/mod.rs
+++ library/std/src/sys/pal/muffin/mod.rs
@@ -1,15 +1,15 @@
-// unsafe extern "C" {
-//     fn main(argc: isize, argv: *const *const u8) -> i32;
-// }
+unsafe extern "C" {
+    fn main(argc: isize, argv: *const *const u8) -> i32;
+}
 
 #[unsafe(no_mangle)]
-pub extern "C" fn _start(_argc: isize, _argv: *const *const u8) -> ! {
-    // let result = unsafe { main(argc, argv) };
+pub extern "C" fn _start(argc: isize, argv: *const *const u8) -> ! {
+    let result = unsafe { main(argc, argv) };
 
     // unsafe {
     //     crate::sys::thread_local::destructors::run();
     // }
     // crate::rt::thread_cleanup();
 
-    crate::sys::os::exit(0);
+    crate::sys::os::exit(result);
 }
